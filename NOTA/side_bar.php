f2a68ca0686db9f7491c4cc2da303128aff3cb9b
Lucifer732 <75311096+Lucifer732@users.noreply.github.com>
2021-08-14 12:18:54 +0530

    Add files via upload

---
+++ NOTA/side_bar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-default navbar-static-top navbar-primary navbar-fixed" role="navigation" style="margin-bottom:6px;background-color:#5cb85c;color:#0022ff;">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/projects/NOTA-Automated/index.html" style = "color:white;"><i class = "fa fa-home fa-large" > </i> NOTA Automated Voting Platform</a>
+
+            </div>
+
+
+            <ul class="nav navbar-top-links navbar-right">
+
+				<?php
+					require 'admin/dbcon.php';
+					$query = $conn->query("SELECT * from voters where voters_id ='{$_SESSION['voters_id']}'")or die (mysqli_errno ());
+					$row = $query->fetch_array();
+				?>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" style = "color:white;font-size:14pt;">
+						<i class="fa fa-arrow fa-fw"></i>Welcome: <?php echo $row['firstname']." ".$row['lastname'];?>
+					</a>
+                </li>
+            </ul>
